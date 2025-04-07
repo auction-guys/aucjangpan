acb1460bc9e9f878271ffb4b6dcdb834b8d9d8c0
Doritos38 <mydjkim@naver.com>
2025-04-08 01:56:28 +0900

    Feat: 주문 목록 조회 구현

--- src/main/java/com/fifteen/auction/domain/order/repository/OrderRepository.java
+++ src/main/java/com/fifteen/auction/domain/order/repository/OrderRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Page<Order> findByUser(User user, Pageable pageable);
