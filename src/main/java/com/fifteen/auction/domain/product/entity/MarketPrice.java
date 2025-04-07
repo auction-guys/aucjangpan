351133d29d087eb6d6050ca61edcbae74a9f1a00
구로불주먹 <whdgns1130@naver.com>
2025-04-07 14:53:40 +0900

    feat:시세조회 entity, repository 추가 (#6)
    * feat:시세조회 entity, repository 추가
    
    * refactor: BaseEntity 상속
    
    * refactor: 세미클론제거

---
+++ src/main/java/com/fifteen/auction/domain/product/entity/MarketPrice.java
@@ -0,0 +1,24 @@
+package com.fifteen.auction.domain.product.entity;
+
+import com.fifteen.auction.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class MarketPrice extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    private Long minMarketPrice;
+    private Long maxMarketPrice;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
