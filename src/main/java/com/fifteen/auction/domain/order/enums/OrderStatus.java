e8187d7b015ed4bf579db5251a297736248d2615
Doritos38 <162943327+Doritos38@users.noreply.github.com>
2025-04-07 14:50:35 +0900

    Feat/payment 결제 관련 entity, repository 추가 (#7)
    * Feat: 결제 관련 entity, repository 추가
    
    * Fix: 엔티티 수정
    
    코드 컨벤션에 맞게 필드 네임 수정, Enum 타입에 Enumerated을 명시

---
+++ src/main/java/com/fifteen/auction/domain/order/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.fifteen.auction.domain.order.enums;
+
+public enum OrderStatus {
+    PENDING, PAID, COMPLETED, CANCLED, REFUNDED
+}
---
+++ src/main/java/com/fifteen/auction/domain/settlement/enums/SettlementStatus.java
@@ -0,0 +1,5 @@
+package com.fifteen.auction.domain.settlement.enums;
+
+public enum SettlementStatus {
+    PENDING, IN_PROGRESS, COMPLETED, FAILED
+}
