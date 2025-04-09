6b3b7d194df8915307a056187f16335100aea676
ParkKyoungMin <happypom88@gmail.com>
2025-04-09 13:37:55 +0900

    fix : product repository 수정

--- src/main/java/com/fifteen/auction/domain/product/repository/ProductRepository.java
+++ src/main/java/com/fifteen/auction/domain/product/repository/ProductRepository.java
@@ -2,11 +2,17 @@ package com.fifteen.auction.domain.product.repository;
 
 import com.fifteen.auction.domain.product.entity.Product;
 import com.fifteen.auction.domain.product.entity.ProductCategory;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAllBySellerId(Long sellerId);
-    List<Product> findByCategory(ProductCategory category);
+    Page<Product> findAllBySellerIdAndDeletedFalse(Long sellerId, Pageable pageable);
+    List<Product> findByCategoryAndDeletedFalse(ProductCategory category);
+    Optional<Product> findByIdAndDeletedFalse(Long id);
+    Page<Product> findAllByDeletedFalse(Pageable pageable);
 }
