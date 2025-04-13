a7f0f92448bc2d491b6e775d317945a2c0740c3b
김종연 <bellopenerr@gmail.com>
2025-04-13 18:31:16 +0900

    chore: auction repository 디렉터리 분리, 이름 변경

--- src/main/java/com/fifteen/auction/domain/auction/repository/auction/AuctionCustomRepository.java
+++ src/main/java/com/fifteen/auction/domain/auction/repository/auction/AuctionCustomRepository.java
@@ -1,4 +1,4 @@
-package com.fifteen.auction.domain.auction.repository;
+package com.fifteen.auction.domain.auction.repository.auction;
 
 import com.fifteen.auction.domain.auction.dto.response.AuctionListItem;
 import com.fifteen.auction.domain.auction.entity.Auction;
