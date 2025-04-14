c3177d7fcfe61232150283f4332c0833dd9b1aa9
ParkKyoungMin <happypom88@gmail.com>
2025-04-14 09:26:27 +0900

    refactor(favorite) : 찜하기 기능 찜 하기, 찜 하기 취소 분리, chore(favorite) : 페이징 추가

--- src/main/java/com/fifteen/auction/domain/favorite/repository/FavoriteRepository.java
+++ src/main/java/com/fifteen/auction/domain/favorite/repository/FavoriteRepository.java
@@ -1,24 +1,24 @@
 package com.fifteen.auction.domain.favorite.repository;
 
 import com.fifteen.auction.domain.favorite.entity.Favorite;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
-
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 
     Optional<Favorite> findByUserIdAndAuctionId(Long userId, Long auctionId);
 
     boolean existsByUserIdAndAuctionId(Long userId, Long auctionId);
 
-    List<Favorite> findAllByUserId(Long userId);
-
     void deleteByUserIdAndAuctionId(Long userId, Long auctionId);
 
-    @Query("SELECT f FROM Favorite f JOIN FETCH f.user JOIN FETCH f.auction WHERE f.user.id = :userId")
-    List<Favorite> findAllWithUserAndAuctionByUserId(@Param("userId") Long userId);
+    @Query("SELECT f FROM Favorite f JOIN FETCH f.auction WHERE f.user.id = :userId")
+    Page<Favorite> findAllWithAuctionByUserId(@Param("userId") Long userId, Pageable pageable);
 
+    @Query("SELECT COUNT(f) FROM Favorite f WHERE f.auction.id = :auctionId")
+    long countByAuctionId(@Param("auctionId") Long auctionId);
 }
