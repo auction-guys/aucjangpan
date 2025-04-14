22d5917e15a48239521a595fc21bf6af0c77b1fb
Doritos38 <mydjkim@naver.com>
2025-04-14 18:09:16 +0900

    Rebase dev

--- src/main/java/com/fifteen/auction/domain/order/repository/OrderRepository.java
+++ src/main/java/com/fifteen/auction/domain/order/repository/OrderRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 
 public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
 
