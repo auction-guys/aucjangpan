e74c7517c976635b8d199789043570d727c84606
friedSaewoo <zriag@naver.com>
2025-04-17 13:57:32 +0900

    feat(chat): Stomp를 사용한 1:1 실시간 채팅 기능 구현 - 실시간채팅 테스트 엔드포인트 추가 - WebSocket 통신시 jwt 인증 설정 추가 - WebSocket 관련 SecurityConfig에서 permitAll(StompHandler에서 jwt 인증)

--- src/main/java/com/fifteen/auction/domain/chat/repository/ChatMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fifteen.auction.domain.chat.repository;
-
-import com.fifteen.auction.domain.chat.entity.ChatMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-}
