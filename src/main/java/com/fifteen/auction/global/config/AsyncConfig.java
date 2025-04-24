e533a759922ca319a67467552886cdb38615de1f
Doritos38 <mydjkim@naver.com>
2025-04-24 10:11:15 +0900

    feat(Payment): 웹훅 기능 구현

--- src/main/java/com/fifteen/auction/global/config/AsyncConfig.java
+++ src/main/java/com/fifteen/auction/global/config/AsyncConfig.java
@@ -22,4 +22,17 @@ public class AsyncConfig {
         executor.initialize();
         return executor;
     }
+
+    @Bean(name = "customWebhookExecutor")
+    public Executor tossExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(30);                                                        // 최소 스레드
+        executor.setMaxPoolSize(60);                                                        // 최대 스레드
+        executor.setQueueCapacity(100);                                                     // 작업 대기열 최대 크기
+        executor.setAllowCoreThreadTimeOut(true);                                           // 유휴 상태가 아래 설정 지나면 스레드 제거
+        executor.setKeepAliveSeconds(300);                                                  // 유휴 상태 시간 설정
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());    // 대기열 초과시 처리 방침
+        executor.initialize();
+        return executor;
+    }
 }
