2cd667d7574d971109be94c47e941e23210bc3d6
김종연 <bellopenerr@gmail.com>
2025-04-28 14:16:20 +0900

    fix: rabbitmq ack 문제 해소

--- src/main/java/com/fifteen/auction/domain/auction/infrastructure/RabbitBidRequestListener.java
+++ src/main/java/com/fifteen/auction/domain/auction/infrastructure/RabbitBidRequestListener.java
@@ -3,7 +3,6 @@ package com.fifteen.auction.domain.auction.infrastructure;
 import com.fifteen.auction.domain.auction.dto.event.BidRequestEvent;
 import com.fifteen.auction.domain.auction.dto.event.BuyNowRequestEvent;
 import com.fifteen.auction.domain.auction.service.port.in.BidEventHandler;
-import com.rabbitmq.client.Channel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
@@ -24,28 +23,22 @@ public class RabbitBidRequestListener {
     private final Jackson2JsonMessageConverter messageConverter;
 
     @RabbitListener(queues = "#{rabbitConfig.queueNames}")
-    public void auctionParticipationListener(Message message, Channel channel) throws IOException {
+    public void auctionParticipationListener(Message message) throws IOException {
         String typeId = message.getMessageProperties().getHeader(TYPE_ID_HEADER_KEY);
         Object payload = messageConverter.fromMessage(message);
 
-        try {
-            switch (typeId) {
-                case TYPE_ID_BID_REQUEST_EVENT -> {
-                    BidRequestEvent e = (BidRequestEvent) payload;
-                    log.info("[입찰 큐 이벤트 처리] auctionSeq={}\tbidderId={}\tbidPrice={}", e.getAuctionSeq(), e.getUserId(), e.getBidPrice());
-                    bidEventHandler.handleBidFromQueue(e.getAuctionSeq(), e.getUserId(), e.getBidPrice());
-                }
-                case TYPE_ID_BUY_NOW_REQUEST_EVENT -> {
-                    BuyNowRequestEvent e = (BuyNowRequestEvent) payload;
-                    log.info("[즉시 구매 큐 이벤트 처리] auctionSeq={}\tbidderId={}\tbidPrice={}", e.getAuctionSeq(), e.getUserId(), e.getBidPrice());
-                    bidEventHandler.handleBuyNowFromQueue(e.getAuctionSeq(), e.getUserId());
-                }
-                default -> log.warn("[RabbitBidRequestListener] 알 수 없는 이벤트 타입={}", typeId);
+        switch (typeId) {
+            case TYPE_ID_BID_REQUEST_EVENT -> {
+                BidRequestEvent e = (BidRequestEvent) payload;
+                log.info("[입찰 큐 이벤트 처리] auctionSeq={}\tbidderId={}\tbidPrice={}", e.getAuctionSeq(), e.getUserId(), e.getBidPrice());
+                bidEventHandler.handleBidFromQueue(e.getAuctionSeq(), e.getUserId(), e.getBidPrice());
             }
-            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
-        } catch (Exception ex) {
-            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
-            throw ex;
+            case TYPE_ID_BUY_NOW_REQUEST_EVENT -> {
+                BuyNowRequestEvent e = (BuyNowRequestEvent) payload;
+                log.info("[즉시 구매 큐 이벤트 처리] auctionSeq={}\tbidderId={}\tbidPrice={}", e.getAuctionSeq(), e.getUserId(), e.getBidPrice());
+                bidEventHandler.handleBuyNowFromQueue(e.getAuctionSeq(), e.getUserId());
+            }
+            default -> log.warn("[RabbitBidRequestListener] 알 수 없는 이벤트 타입={}", typeId);
         }
     }
 }
