8f46817e08998d8bf6b253b8d6696e9586705b44
Kim su-hwan <sshkim1013@naver.com>
2025-05-04 16:06:59 +0900

    fix(user): 테스트 코드 오류 수정.

--- src/test/java/com/fifteen/auction/domain/user/auth/controller/AuthControllerTest.java
+++ src/test/java/com/fifteen/auction/domain/user/auth/controller/AuthControllerTest.java
@@ -30,7 +30,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@ActiveProfiles("local")
+@ActiveProfiles("prod")
 @AutoConfigureMockMvc
 class AuthControllerTest {
 
