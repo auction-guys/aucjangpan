78473bb8877cc460b894d83d29477296faf63f69
Kim su-hwan <sshkim1013@naver.com>
2025-05-05 19:59:55 +0900

    fix(user): 테스트 코드 오류 수정.

--- src/test/java/com/fifteen/auction/domain/user/auth/controller/AuthControllerTest.java
+++ src/test/java/com/fifteen/auction/domain/user/auth/controller/AuthControllerTest.java
@@ -18,12 +18,9 @@ import org.mockito.Mockito;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
