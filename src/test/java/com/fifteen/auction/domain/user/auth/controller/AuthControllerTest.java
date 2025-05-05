f24978c268caae5904f5c1c10832a968c5fc8946
Kim su-hwan <sshkim1013@naver.com>
2025-05-05 17:35:29 +0900

    fix(user): 테스트 코드 오류 수정.

--- src/test/java/com/fifteen/auction/domain/user/auth/controller/AuthControllerTest.java
+++ src/test/java/com/fifteen/auction/domain/user/auth/controller/AuthControllerTest.java
@@ -17,7 +17,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -25,13 +27,15 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@ActiveProfiles("prod")
-@AutoConfigureMockMvc
+@WebMvcTest(
+        controllers = AuthController.class,
+        excludeAutoConfiguration = SecurityAutoConfiguration.class
+)
 class AuthControllerTest {
 
     @Autowired
@@ -45,6 +49,9 @@ class AuthControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @MockBean
+    private JwtUtil jwtUtil;
 
     @Test
     void 회원가입_성공() throws Exception {
@@ -67,7 +74,8 @@ class AuthControllerTest {
 
         mockMvc.perform(post("/api/v1/auth/signup")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonContent))
+                        .content(jsonContent)
+                        .with(csrf()))
                 .andExpect(status().isCreated());
     }
 
